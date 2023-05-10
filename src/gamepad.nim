0a1b4b6515136d8630e24840fafb0e82de68fd3d
David Konsumer <konsumer@jetboystudio.com>
2023-05-10 15:20:48 -0700

    try again with windows, maybe just the dinput part?

--- src/gamepad.nim
+++ src/gamepad.nim
@@ -11,7 +11,7 @@ when defined(linux):
 
 when defined(windows):
   {.compile: "src/libstem_gamepad/source/gamepad/Gamepad_windows_dinput.c".}
-  {.compile: "src/libstem_gamepad/source/gamepad/Gamepad_windows_mm.c".}
+  # {.compile: "src/libstem_gamepad/source/gamepad/Gamepad_windows_mm.c".}
   {.passC: "-DFREEGLUT_STATIC".}
   {.passL: "-lXinput -ldinput8 -ldxguid -l/WbemUuid -lOle32 -lOleAut32".}
 
